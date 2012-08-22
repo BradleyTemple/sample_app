20a3fa41cac79cad245be93831e08c424f5bec1c
Bradley Temple <bradley@bradley-online.com>
2012-08-21 21:29:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PDF Sample Application
+
+This sample application will be useful in experimenting with various automation techniques with regard to PDFs.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PdfSampleApp::Application.config.secret_token = 'fe28152eb14eb9ff4c203135227848135d30ba52d9fcb11ae486edecb9bb7cea2cc54599a50e64b1ca10f4d00ca7157d07cb2dc1c323c4913418b11bc04115ce'
